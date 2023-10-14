db4909e4e6e834c4c9474f9ffb019e444667445c
stum0 <70952638+stum0@users.noreply.github.com>
2023-10-14 17:50:01 +0100

    test with just javascript 2

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,8 @@
 #![allow(clippy::type_complexity)]
 
 use bevy::prelude::*;
-use gloo::events;
-use gloo::{events::EventListener, timers::callback::Timeout};
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsValue;
 
-use web_sys::window;
+use wasm_bindgen::prelude::*;
 
 fn main() {
     App::new()
@@ -21,12 +16,14 @@ fn main() {
                 title: "cliptest".to_string(),
                 fit_canvas_to_parent: true,
                 prevent_default_event_handling: false,
+
                 ..default()
             }),
             ..default()
         }))
         .add_systems(Startup, setup)
         .add_systems(Update, button_system)
+        .insert_resource(ClearColor(Color::WHITE))
         .run();
 }
 
@@ -45,101 +42,84 @@ fn button_system(
         (Changed<Interaction>, With<Button>),
     >,
     mut text_query: Query<&mut Text>,
-    mouse: Res<Input<MouseButton>>,
-    touches: Res<Touches>,
 ) {
+    for (interaction, mut color, mut border_color, children) in &mut interaction_query {
+        let mut text = text_query.get_mut(children[0]).unwrap();
 
-    // for (interaction, mut color, mut border_color, children) in &mut interaction_query {
-    //     let mut text = text_query.get_mut(children[0]).unwrap();
-    //     if mouse.just_pressed(MouseButton::Left) || mouse.just_pressed(MouseButton::Right) {
-    //         copyToClipboard("Text to be copied");
-    //     }
-    //     for touch in touches.iter_just_pressed() {
-    //         copyToClipboard("Text to be copied");
-    //     }
-    //     // match *interaction {
-    //     //     Interaction::Pressed => {
-    //     //         text.sections[0].value = "Press".to_string();
-    //     //         *color = PRESSED_BUTTON.into();
-    //     //         border_color.0 = Color::RED;
+        match *interaction {
+            Interaction::Pressed => {
+                text.sections[0].value = "Press".to_string();
+                *color = PRESSED_BUTTON.into();
+                border_color.0 = Color::RED;
 
-    //     //         copyToClipboard("Text to be copied");
-    //     //     }
-    //     //     Interaction::Hovered => {
-    //     //         text.sections[0].value = "Hover".to_string();
-    //     //         *color = HOVERED_BUTTON.into();
-    //     //         border_color.0 = Color::WHITE;
-    //     //     }
-    //     //     Interaction::None => {
-    //     //         text.sections[0].value = "Button".to_string();
-    //     //         *color = NORMAL_BUTTON.into();
-    //     //         border_color.0 = Color::BLACK;
-    //     //     }
-    //     // }
-    // }
+                check_player_connection_and_hide_button(true);
+            }
+            Interaction::Hovered => {
+                text.sections[0].value = "Hover".to_string();
+                *color = HOVERED_BUTTON.into();
+                border_color.0 = Color::WHITE;
+            }
+            Interaction::None => {
+                text.sections[0].value = "Button".to_string();
+                *color = NORMAL_BUTTON.into();
+                border_color.0 = Color::BLACK;
+            }
+        }
+    }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // // ui camera
-    // commands.spawn(Camera2dBundle::default());
-    // commands
-    //     .spawn(NodeBundle {
-    //         style: Style {
-    //             width: Val::Percent(100.0),
-    //             height: Val::Percent(100.0),
-    //             align_items: AlignItems::Center,
-    //             justify_content: JustifyContent::Center,
-    //             ..default()
-    //         },
-    //         ..default()
-    //     })
-    //     .with_children(|parent| {
-    //         parent
-    //             .spawn(ButtonBundle {
-    //                 style: Style {
-    //                     width: Val::Px(150.0),
-    //                     height: Val::Px(65.0),
-    //                     border: UiRect::all(Val::Px(5.0)),
-    //                     // horizontally center child text
-    //                     justify_content: JustifyContent::Center,
-    //                     // vertically center child text
-    //                     align_items: AlignItems::Center,
-    //                     ..default()
-    //                 },
-    //                 border_color: BorderColor(Color::BLACK),
-    //                 background_color: NORMAL_BUTTON.into(),
-    //                 ..default()
-    //             })
-    //             .with_children(|parent| {
-    //                 parent.spawn(TextBundle::from_section(
-    //                     "Button",
-    //                     TextStyle {
-    //                         font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-    //                         font_size: 40.0,
-    //                         color: Color::rgb(0.9, 0.9, 0.9),
-    //                     },
-    //                 ));
-    //             });
-    //     });
+    // ui camera
+    commands.spawn(Camera2dBundle::default());
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                align_items: AlignItems::Center,
+                justify_content: JustifyContent::Center,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            parent
+                .spawn(ButtonBundle {
+                    style: Style {
+                        width: Val::Px(150.0),
+                        height: Val::Px(65.0),
+                        border: UiRect::all(Val::Px(5.0)),
+                        // horizontally center child text
+                        justify_content: JustifyContent::Center,
+                        // vertically center child text
+                        align_items: AlignItems::Center,
+                        ..default()
+                    },
+                    border_color: BorderColor(Color::BLACK),
+                    background_color: NORMAL_BUTTON.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from_section(
+                        "Button",
+                        TextStyle {
+                            font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                            font_size: 40.0,
+                            color: Color::rgb(0.9, 0.9, 0.9),
+                        },
+                    ));
+                });
+        });
 }
-
-// #[cfg(target_arch = "wasm32")]
-// fn copy_to_clipboard(text: &str) {
-//     #[wasm_bindgen]
-//     extern "C" {
-//         #[wasm_bindgen(js_namespace = ["navigator", "clipboard"], js_name = writeText)]
-//         fn write_text(s: &str);
-//     }
-
-//     write_text(text);
-// }
 
 #[wasm_bindgen]
 extern "C" {
-    fn copyToClipboard(text: &str);
+    fn hideCopyButton();
 }
 
 #[wasm_bindgen]
-pub fn copy_text(text: &str) {
-    copyToClipboard(text);
+pub fn check_player_connection_and_hide_button(player_connected: bool) {
+    if player_connected {
+        hideCopyButton();
+    }
 }
